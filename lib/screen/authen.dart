7fbc0793b58112ad1ec76a89b7f5550d75f075eb
suttiruk0906 <suttiruk0906@gmail.com>
2020-09-21 16:49:57 +0700

    Userpassword

--- lib/screen/authen.dart
+++ lib/screen/authen.dart
@@ -31,6 +31,54 @@ class _AuthenState extends State<Authen> {
     );
   }
 
+  Widget emailTextFormfeild() {
+    return Container(
+      width: size,
+      child: TextFormField(
+        style: TextStyle(
+          color: Colors.orange[600],
+        ),
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            icon: Icon(
+              Icons.email,
+              size: 30.0,
+              color: Colors.orange[600],
+            ),
+            labelText: 'User',
+            labelStyle: TextStyle(
+              color: Colors.orange[600],
+            ),
+            hintText: 'abc@email.com'),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        obscureText: true, //secure password
+        style: TextStyle(
+          color: Colors.orange[600],
+        ),
+        keyboardType: TextInputType.text, //keyboard style
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.lock,
+            size: 30.0,
+            color: Colors.orange[600],
+          ),
+          labelText: 'Password : ',
+          labelStyle: TextStyle(
+            color: Colors.orange[600],
+          ),
+          hintText: 'More 6 Charactor',
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +86,7 @@ class _AuthenState extends State<Authen> {
       alignment: Alignment(0, -1),
       padding: EdgeInsets.only(top: 70.0),
       child: Column(
-        children: <Widget>[Text(''),showLogo(),showname()],
+        children: <Widget>[showLogo(), showname(),emailTextFormfeild(),passwordText()],
       ),
     ));
   }
